5765e905468d1ae9aed676d0f7e3bd86c86b7333
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-05-28 18:45:26 +0300

    35%

---
+++ src/js/swiper.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.mySwiper', {
+  effect: 'flip',
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
---
+++ src/js/tippy.js
@@ -0,0 +1,10 @@
+// tippy('[data-tippy-content]');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   tippy('.button', {
+//     allowHTML: true,
+//     theme: 'light',
+//     interactive: true, // Дозволяє взаємодіювати з тултипом
+//     trigger: 'click', // Відображення тултипу при кліку
+//   });
+// });
