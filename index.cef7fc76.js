213818db334ac99e8e65287af60d2138b606cce0
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2024-05-29 12:41:08 +0000

    Deploying to gh-pages from @ DmytroKrylov/Naturehike@75f9010319ee1af4c7696ff29413e6db86cad1fc 🚀

--- index.cef7fc76.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{effect:"flip",grabCursor:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.cef7fc76.js.map
