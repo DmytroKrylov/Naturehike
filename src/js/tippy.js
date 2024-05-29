ec42f78f5e0dbf4ffe739cc89b4d1d5521e479e5
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-05-29 12:51:24 +0300

    40%

--- src/js/tippy.js
+++ src/js/tippy.js
@@ -1,10 +1,14 @@
-// tippy('[data-tippy-content]');
+document.addEventListener('DOMContentLoaded', function () {
+  tippy('[data-tippy-content]', {
+    trigger: 'click',
+    interactive: true,
+    allowHTML: true,
+    theme: 'light', // Optional: you can customize the theme
+  });
+});
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   tippy('.button', {
-//     allowHTML: true,
-//     theme: 'light',
-//     interactive: true, // Дозволяє взаємодіювати з тултипом
-//     trigger: 'click', // Відображення тултипу при кліку
-//   });
-// });
+document.addEventListener('DOMContentLoaded', function () {
+  tippy('[data-tippy-content]', {
+    allowHTML: true,
+  });
+});
